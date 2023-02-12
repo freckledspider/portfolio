339df3de03ca2654a265831b1a133ffd466b04f6
bri <bcamesi@gmail.com>
2023-02-11 22:45:22 -0500

    index page created, basic components added

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (props) => {
+  return (
+    <div className="footer">
+        Footer content here
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+  return (
+    <div className="nav">
+      <Link to="/">
+        Home
+      </Link>
+    </div>
+  );
+};
+
+export default Nav;
