aca4c2c53073f2ee00d642ee14779a96a3b07b84
bri <bcamesi@gmail.com>
2023-02-13 18:51:38 -0500

    styling changes and adding images

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,17 +1,19 @@
 import { HashLink as Link } from "react-router-hash-link";
+import bcheader from "../../src/bcheader.png"
 
 const Main = (props) => {
     return <>
 <div className="header">
     <div className="nav">
     <ul>
-    <li><Link to="/">Home</Link></li>
+    <li><Link to="#top">Home</Link></li>
     <li><Link to="#about">About</Link></li>
     <li><Link to="#projects">Projects</Link></li>
     <li><Link to="#contact">Contact</Link></li>
+    <li><Link to="https://docs.google.com/document/d/1I_atmT0EHkD7Jz5G2tXVbIPgGBlGOfOV9rOQMPNK3MU/edit?usp=sharing" target="_blank">Resume</Link></li>
     </ul>
     </div>
-      <h1>Brianne Camesi</h1>
+      <img src={bcheader} alt="Brianne Camesi Full-Stack Web Developer" width="800"/>
     </div>
 </>
   };
