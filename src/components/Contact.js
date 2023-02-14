90f5b7a0e47629706cf5fa498b9760bd442f4947
bri <bcamesi@gmail.com>
2023-02-14 17:14:14 -0500

    contact component updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ const Contact = (props) => {
     return <>
 <div id="contact" className="contact">
   <h2>Contact</h2>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  <br/><br/>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  Let's get in touch! Please don't hesitate to reach out for any reason. I love connecting with others who are also passionate about tech.<br/><br/>
+  <a href="mailto:brianne.camesi@gmail.com" target="_blank" rel="noreferrer">brianne.camesi@gmail.com</a> | <a href="https://www.linkedin.com/in/briannecamesi/" target="_blank" rel="noreferrer">LinkedIn</a> | <a href="https://github.com/freckledspider" target="_blank" rel="noreferrer">GitHub</a><br/><br/>
+  This website was made by scratch by Brianne Camesi with the help of react and free to use unsplash images edited in Photoshop.
 </div>
 </>
   };
