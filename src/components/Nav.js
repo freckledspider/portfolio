c5cad4195592e2648d775e2711ff6206fad6aeb1
bri <bcamesi@gmail.com>
2024-03-26 20:42:14 -0400

    hamburgermenu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,66 @@
+import React, { useState } from "react"; // Import useState
+import Hamburger from "../images/hamburger.png";
+
 export default function Nav() {
+  // State to manage the visibility of the hamburger menu links
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <div>
         <div className="nav">
-          <div className="navtitle">
-           Bri Camesi
+          <div className="navtitle">Bri Camesi</div>
+          <div className="hamburgermenu">
+            {" "}
+            {/* Add onClick event */}
+            <img
+              className="hamburgericon"
+              src={Hamburger}
+              alt="Menu"
+              onClick={toggleMenu}
+            />
+            {/* Conditionally render the links based on isMenuOpen state */}
+            {isMenuOpen && (
+              <div className="hamburgermenulinks">
+                <ul className="hamburgerlist">
+                  <li className="hamburgerli">
+                    <a href="/">Home</a>
+                  </li>
+                  <li className="hamburgerli">
+                    <a href="/about">About/Skills</a>
+                  </li>
+                  <li className="hamburgerli">
+                    <a href="/projects">Projects</a>
+                  </li>
+                  <li className="hamburgerli">
+                    <a href="/contact">Contact</a>
+                  </li>
+                  <li className="hamburgerli">
+                    <a
+                      href="https://github.com/freckledspider"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      GitHub
+                    </a>
+                  </li>
+                  <li className="hamburgerli">
+                    <a
+                      href="https://www.linkedin.com/in/briannecamesi/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      LinkedIn
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
           <div className="navlinks">
             <ul className="navigationlist">
@@ -21,12 +77,20 @@ export default function Nav() {
                 <a href="/contact">Contact</a>
               </li>
               <li className="navigationli">
-                <a href="https://github.com/freckledspider" target="_blank" rel="noreferrer">
+                <a
+                  href="https://github.com/freckledspider"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   GitHub
                 </a>
               </li>
               <li className="navigationli">
-                <a href="https://www.linkedin.com/in/briannecamesi/" target="_blank" rel="noreferrer">
+                <a
+                  href="https://www.linkedin.com/in/briannecamesi/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   LinkedIn
                 </a>
               </li>
