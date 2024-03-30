b111575ae308427523350965bbccbd7a9c0641a7
bri <bcamesi@gmail.com>
2024-03-29 21:14:26 -0400

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+    <Analytics/>
     <Nav />
     <App />
     <Footer />
-    <Analytics/>
     </Router>
   </React.StrictMode>
 );
